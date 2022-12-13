a7c8618e8161e2c55e6cbf71af0bbec9d428b8d9
Niek <54935153+NiekSulter@users.noreply.github.com>
2022-12-13 16:31:37 +0100

    single/multifile example

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from EDFParser import EDFParser
+import glob
 
 
-def main():
+def single_file():
     path = "/Volumes/Databases/TUH_EEG/v3.0.0/edf/train/normal/01_tcp_ar/aaaaaanr_s003_t000.edf"
     
     EDF = EDFParser()
     z = EDF.parse_file(path)
     print(z)
-        
-        
-if __name__ == '__main__':
-     main()
+
+
+def multi_file():
+    path_list = [path for path in glob.iglob('/Volumes/Databases/TUH_EEG/v3.0.0/edf/train/normal/01_tcp_ar/*.edf')]
+    
+    EDF = EDFParser()
+    for path in path_list:
+        z = EDF.parse_file(path)
+        print(z)
