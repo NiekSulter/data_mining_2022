7f03fb8ac6fde9141e77a22c052cd4ee00251ca0
Lean <lean.schoonveld11@gmail.com>
2022-12-30 15:25:15 +0100

    fixed bug that lead to bad results, and a lot of other bugs

--- decision_tree_classifier.py
+++ decision_tree_classifier.py
@@ -6,8 +6,6 @@ from sklearn.model_selection import train_test_split
 from collections import Counter
 from sklearn.metrics import accuracy_score
 
-#https://dafriedman97.github.io/mlbook/content/c6/s2/boosting.html
-
 
 def read_edf(path):
     signals, signal_headers, header = highlevel.read_edf(path)
@@ -15,17 +13,17 @@ def read_edf(path):
 
 # write a decision tree classifier
 # https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
-
-
 # https://betterdatascience.com/mml-decision-trees/
 
 class Node:
+    """Single node of a decision tree
+    """
     # a single node of a decision tree
     def __init__(self, feature=None, threshold=None, left=None, right=None, gain=None, value=None):
-        self.feature = feature
-        self.threshold = threshold
         self.left = left
         self.right = right
+        self.feature = feature
+        self.threshold = threshold
         self.gain = gain
         self.value = value
 
@@ -39,6 +37,14 @@ class DecisionTree:
 
     @staticmethod
     def _entropy(s):
+        """Calculates the entropy of a list of values. The entropy is
+        calculated using the formula: -sum(p(x)log(p(x))) where p(x) is
+        the probability of each unique value in the list.
+        Args:
+            s (numpy.ndarray): values
+        Returns:
+            float: entropy
+        """
         counts = np.bincount(np.array(s, dtype=np.int64))
         # bincount returns the number of occurrences of each value in an array of non-negative ints
         perct = counts / len(s)
@@ -49,28 +55,37 @@ class DecisionTree:
                 entropy -= pct * np.log2(pct)
         return entropy
 
+
     def _information_gain(self, parent, left, right):
         """Calculates the information gain. From the parent and two
         child nodes, the information gain is calculated using the 
-        formula: entropy(parent) - (left_c * entropy(left) + right_c * entropy(right))
+        formula: entropy(parent) - ((left_c * entropy(left) + right_c * entropy(right)))
         Args:
-            parent (_type_): _description_
-            left (_type_): _description_
-            right (_type_): _description_
-
+            parent (numpy.ndarray): parent node
+            left (numpy.ndarray): left child node
+            right (numpy.ndarray): right child node
         Returns:
-            _type_: _description_
+            float: information gain
         """
         # parent is the entropy of the parent node
         # left and right are the entropy of the left and right child nodes
         # parent - (left + right) is the information gain
         left_c = len(left) / len(parent)
         right_c = len(right) / len(parent)
-        gain = self._entropy(parent) - (left_c * self._entropy(left)) + (right_c * self._entropy(right))
+        gain = self._entropy(parent) - ((left_c * self._entropy(left)) + right_c * self._entropy(right))
         return gain # can one be a one liner. 
 
 
     def _best_split(self, X, y):
+        """Calculates the best split for a for a feature and target.
+        Args:
+            X (numpy.ndarray): features
+            y (numpy.ndarray): target
+
+        Returns:
+            dict: best split dictionary with features, treshold,
+                gain, left and right
+        """
         best_split = {
             "feature": None,
             "threshold": None, 
@@ -79,11 +94,11 @@ class DecisionTree:
             "right": None
         }
         best_gain = -1
-        for feature in range(X.shape[1]):
+        for feature in range(X.shape[1]): # in columns
             # every unique of the feature
-            uniq_f = np.unique(X[:, feature])
-            for threshold in uniq_f:
-                # create a new df and split it into left andright
+            # uniq_f = np.unique(X[:, feature])
+            for threshold in np.unique(X[:, feature]):
+                # create a new df and split it into left and right
                 # right includes records higher than the threshold
                 df = np.concatenate((X, y.reshape(1, -1).T), axis=1)
                 left = np.array([row for row in df if row[feature] <= threshold])
@@ -93,88 +108,92 @@ class DecisionTree:
                 # there are records in both left and right
                 if len(left) > 0 and len(right) > 0:
                     y = df[:, -1]
-                    y_left = left[:, -1]
-                    y_right = right[:, -1]
-                    gain = self._information_gain(y, y_left, y_right)
+                    gain = self._information_gain(y, left[:,-1], right[:,-1])
                     if gain > best_gain:
-                        best_gain = gain
                         best_split["feature"] = feature
                         best_split["threshold"] = threshold
                         best_split["gain"] = gain
                         best_split["left"] = left
                         best_split["right"] = right
+                        best_gain = gain
         return best_split
 
     def _build_tree(self, X, y, depth=0):
         """Builds the decision tree by finding the best split for a 
         dataset and then recursively building the left and right child
         nodes.
-
         Args:
-            X (_type_): _description_
-            y (_type_): _description_
-            depth (int, optional): _description_. Defaults to 0.
-
+            X (numpy.ndarray): features
+            y (numpy.ndarray): target
+            depth (int, optional): current_depth. Defaults to 0.
         Returns:
-            _type_: _description_
+            Node: node
         """
         # find the best split
-        split = self._best_split(X, y)
         if X.shape[0] >= self.min_samples_split and depth <= self.max_depth:
+            split = self._best_split(X, y)
             if split["gain"] > 0: # to prevent None
-                left = self._build_tree(X=split["left"][:, :-1], y=split["left"][:, -1], depth=depth+1)
-                right = self._build_tree(X=split["right"][:, :-1], y=split["right"][:, -1], depth=depth+1)
-                return Node(feature=split["feature"], threshold=split["threshold"], left=left, right=right, gain=split["gain"])
+                left_n = self._build_tree(X=split["left"][:, :-1], y=split["left"][:, -1], depth=depth+1)
+                right_n = self._build_tree(X=split["right"][:, :-1], y=split["right"][:, -1], depth=depth+1)
+                return Node(feature=split["feature"], threshold=split["threshold"], left=left_n, right=right_n, gain=split["gain"])
         return Node(value=Counter(y).most_common(1)[0][0])
 
+
     def fit(self, X, y):
+        """Used to fit the decision tree to the training data.
+        Args:
+            X (np.array): features
+            y (np.array or list): target
+        """
         self.root = self._build_tree(X, y)
 
 
     def _predict(self, x, tree):
-        '''
-        Helper recursive function, used to predict a single instance (tree traversal).
-        
-        :param x: single observation
-        :param tree: built tree
-        :return: float, predicted class
-        '''
-        # Leaf node
-        if tree.value != None:
+        """Used to predict the class of a single observation.
+        Args:
+            x (np.array): single observation
+            tree (Node): decision tree
+        Returns:
+            _type_: _description_
+        """
+        if tree.value is not None:
             return tree.value
-        feature_value = x[tree.feature]
+
+        # x[tree.feature] is the value of the feature in the observation
+        if x[tree.feature] <= tree.threshold:
+            return self._predict(x, tree.left)
+        else:   # to right tree
+            return self._predict(x, tree.right)
         
-        # Go to the left
-        if feature_value <= tree.threshold:
-            return self._predict(x=x, tree=tree.left)
-        
-        # Go to the right
-        if feature_value > tree.threshold:
-            return self._predict(x=x, tree=tree.right)
-        
+    
     def predict(self, X):
-        '''
-        Function used to classify new instances.
-        
-        :param X: np.array, features
-        :return: np.array, predicted classes
-        '''
-        # Call the _predict() function for every observation
-        return [self._predict(x, self.root) for x in X]
+        """Used to predict the class of multiple observations.
+        Args:
+            X (np.array): features
+        Returns:
+            list: predicted classes
+        """
+        preds = []
+        for x in X:
+            # calls itself recursively
+            # TODO list comprehension
+            preds.append(self._predict(x, self.root))
+        return preds
+        # return [self._predict(x, self.root) for x in X]
 
 
 def main():
     # file_edf = "/Users/lean/Library/CloudStorage/OneDrive-Persoonlijk/School/Master/Pre-Master_DataScience_(Minor_Computing_Science)/2223_Data_Mining_(KW1_V)/data_project/aaaaaaff_s002_t000.edf"
     # read_edf(file_edf)
+
+    # tested on the Iris dataset
     iris = load_iris()
-    X = iris["data"]
-    y = iris["target"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    model = DecisionTree()
+    X_train, X_test, y_train, y_test = train_test_split(iris["data"], iris["target"], test_size=0.2, random_state=42)
+    # X_train, X_test, y_train, y_test = train_test_split(iris["data"], iris["target"], test_size=0.2)
+    model = DecisionTree(max_depth=5)
     model.fit(X_train, y_train)
-
     preds = model.predict(X_test)
+    
     print(accuracy_score(y_test, preds))
 
 
--- test.py
+++ test.py
@@ -33,7 +33,6 @@ class DecisionTree:
         :param s: list
         :return: float, entropy value
         '''
-        print(s, "kaaaasas")
         # Convert to integers to avoid runtime errors
         counts = np.bincount(np.array(s, dtype=np.int64))
         # Probabilities of each class label
@@ -55,98 +54,141 @@ class DecisionTree:
         :param right_child: list, right child of a parent
         :return: float, information gain
         '''
+        # print(parent)
         num_left = len(left_child) / len(parent)
         num_right = len(right_child) / len(parent)
         
         # One-liner which implements the previously discussed formula
         return self._entropy(parent) - (num_left * self._entropy(left_child) + num_right * self._entropy(right_child))
-    
-    def _best_split(self, X, y):
-        '''
-        Helper function, calculates the best split for given features and target
-        
-        :param X: np.array, features
-        :param y: np.array or list, target
-        :return: dict
-        '''
-        best_split = {}
-        best_info_gain = -1
-        n_rows, n_cols = X.shape
-        # print(X, y)
-        # For every dataset feature
-        for f_idx in range(n_cols):
-            X_curr = X[:, f_idx]
-            # For every unique value of that feature
-            for threshold in np.unique(X_curr):
-                # Construct a dataset and split it to the left and right parts
-                # Left part includes records lower or equal to the threshold
-                # Right part includes records higher than the threshold
-                df = np.concatenate((X, y.reshape(1, -1).T), axis=1)
-                df_left = np.array([row for row in df if row[f_idx] <= threshold])
-                df_right = np.array([row for row in df if row[f_idx] > threshold])
-                # print(df_left, y, "df_left")
-                # Do the calculation only if there's data in both subsets
-                if len(df_left) > 0 and len(df_right) > 0:
-                    # Obtain the value of the target variable for subsets
-                    y = df[:, -1]
-                    y_left = df_left[:, -1]
-                    y_right = df_right[:, -1]
+    def _informationbn_gain(self, parent, left, right):
+        """Calculates the information gain. From the parent and two
+        child nodes, the information gain is calculated using the 
+        formula: entropy(parent) - (left_c * entropy(left) + right_c * entropy(right))
+        Args:
+            parent (_type_): _description_
+            left (_type_): _description_
+            right (_type_): _description_
 
-                    # Caclulate the information gain and save the split parameters
-                    # if the current split if better then the previous best
-                    # print(y, "y")
-                    gain = self._information_gain(y, y_left, y_right)
-                    if gain > best_info_gain:
-                        best_split = {
-                            'feature_index': f_idx,
-                            'threshold': threshold,
-                            'df_left': df_left,
-                            'df_right': df_right,
-                            'gain': gain
-                        }
-                        best_info_gain = gain
-        return best_split
+        Returns:
+            _type_: _description_
+        """
+        print(parent)
+        # parent is the entropy of the parent node
+        # left and right are the entropy of the left and right child nodes
+        # parent - (left + right) is the information gain
+        left_c = len(left) / len(parent)
+        right_c = len(right) / len(parent)
+        gain = self._entropy(parent) - (left_c * self._entropy(left)) + (right_c * self._entropy(right))
+        return gain # can one be a one liner.        
     
-    def _build(self, X, y, depth=0):
-        '''
-        Helper recursive function, used to build a decision tree from the input data.
+    # def _best_split(self, X, y):
+    #     '''
+    #     Helper function, calculates the best split for given features and target
         
-        :param X: np.array, features
-        :param y: np.array or list, target
-        :param depth: current depth of a tree, used as a stopping criteria
-        :return: Node
-        '''
-        n_rows, n_cols = X.shape
-        
-        # Check to see if a node should be leaf node
-        if n_rows >= self.min_samples_split and depth <= self.max_depth:
-            # Get the best split
-            print(y, "yyyyy")
-            best = self._best_split(X, y)
-            # If the split isn't pure
-            if best['gain'] > 0:
-                # Build a tree on the left
-                left = self._build(
-                    X=best['df_left'][:, :-1], 
-                    y=best['df_left'][:, -1], 
-                    depth=depth + 1
-                )
-                right = self._build(
-                    X=best['df_right'][:, :-1], 
-                    y=best['df_right'][:, -1], 
-                    depth=depth + 1
-                )
-                return Node(
-                    feature=best['feature_index'], 
-                    threshold=best['threshold'], 
-                    data_left=left, 
-                    data_right=right, 
-                    gain=best['gain']
-                )
-        # Leaf node - value is the most common target value 
-        return Node(
-            value=Counter(y).most_common(1)[0][0]
-        )
+    #     :param X: np.array, features
+    #     :param y: np.array or list, target
+    #     :return: dict
+    #     '''
+    #     best_split = {}
+    #     best_info_gain = -1
+    #     # print(X.shape)
+    #     n_rows, n_cols = X.shape
+    #     # print(X, y)
+    #     # For every dataset feature
+    #     for f_idx in range(n_cols):
+    #         X_curr = X[:, f_idx]
+    #         # For every unique value of that feature
+    #         for threshold in np.unique(X_curr):
+    #             # Construct a dataset and split it to the left and right parts
+    #             # Left part includes records lower or equal to the threshold
+    #             # Right part includes records higher than the threshold
+    #             df = np.concatenate((X, y.reshape(1, -1).T), axis=1)
+    #             df_left = np.array([row for row in df if row[f_idx] <= threshold])
+    #             df_right = np.array([row for row in df if row[f_idx] > threshold])
+    #             # print(df_left, y, "df_left")
+    #             # Do the calculation only if there's data in both subsets
+    #             if len(df_left) > 0 and len(df_right) > 0:
+    #                 # Obtain the value of the target variable for subsets
+    #                 y = df[:, -1]
+    #                 y_left = df_left[:, -1]
+    #                 y_right = df_right[:, -1]
+
+    #                 # Caclulate the information gain and save the split parameters
+    #                 # if the current split if better then the previous best
+    #                 # print(y, "y")
+    #                 gain = self._information_gain(y, y_left, y_right)
+    #                 if gain > best_info_gain:
+    #                     best_split = {
+    #                         'feature_index': f_idx,
+    #                         'threshold': threshold,
+    #                         'df_left': df_left,
+    #                         'df_right': df_right,
+    #                         'gain': gain
+    #                     }
+    #                     best_info_gain = gain
+    #     return best_split
+
+
+    def _best_split(self, X, y):
+        best_split = {
+            "feature_index": None,
+            "threshold": None, 
+            "gain": None,
+            "df_left": None,
+            "df_right": None
+        }
+        best_gain = -1
+        # print(X.shape)
+        for feature in range(X.shape[1]): # in columns
+            # every unique of the feature
+            uniq_f = np.unique(X[:, feature])
+            # for threshold in uniq_f:
+            for threshold in np.unique(X[:, feature]):
+                # create a new df and split it into left and right
+                # right includes records higher than the threshold
+                df = np.concatenate((X, y.reshape(1, -1).T), axis=1)
+                left = np.array([row for row in df if row[feature] <= threshold])
+                right = np.array([row for row in df if row[feature] > threshold])
+
+                # only calculate the information gain if \
+                # there are records in both left and right
+                if len(left) > 0 and len(right) > 0:
+                    y = df[:, -1]
+                    # y_left = left[:, -1]
+                    # y_right = right[:, -1]
+                    gain = self._information_gain(y, left[:,-1], right[:,-1])
+                    if gain > best_gain:
+                        best_split["feature_index"] = feature
+                        best_split["threshold"] = threshold
+                        best_split["gain"] = gain
+                        best_split["df_left"] = left
+                        best_split["df_right"] = right
+                        best_gain = gain
+        return best_split    
+    def _build_tree(self, X, y, depth=0):
+        """Builds the decision tree by finding the best split for a 
+        dataset and then recursively building the left and right child
+        nodes.
+
+        Args:
+            X (_type_): _description_
+            y (_type_): _description_
+            depth (int, optional): _description_. Defaults to 0.
+
+        Returns:
+            _type_: _description_
+        """
+        # find the best split
+        # print(split)
+        print(X.shape)
+        if X.shape[0] >= self.min_samples_split and depth <= self.max_depth:
+            split = self._best_split(X, y)
+            # print(split)
+            if split["gain"] > 0: # to prevent None
+                left_n = self._build_tree(X=split["df_left"][:, :-1], y=split["df_left"][:, -1], depth=depth+1)
+                right_n = self._build_tree(X=split["df_right"][:, :-1], y=split["df_right"][:, -1], depth=depth+1)
+                return Node(feature=split["feature_index"], threshold=split["threshold"], data_left=left_n, data_right=right_n, gain=split["gain"])
+        return Node(value=Counter(y).most_common(1)[0][0])
     
     def fit(self, X, y):
         '''
@@ -157,43 +199,45 @@ class DecisionTree:
         :return: None
         '''
         # Call a recursive function to build the tree
-        self.root = self._build(X, y)
+        self.root = self._build_tree(X, y)
         
     def _predict(self, x, tree):
-        '''
-        Helper recursive function, used to predict a single instance (tree traversal).
-        
-        :param x: single observation
-        :param tree: built tree
-        :return: float, predicted class
-        '''
-        # Leaf node
-        if tree.value != None:
+        """Used to predict the class of a single observation.
+        Args:
+            x (np.array): single observation
+            tree (Node): decision tree
+        Returns:
+            _type_: _description_
+        """
+        if tree.value is not None:
             return tree.value
-        feature_value = x[tree.feature]
+
+        # x[tree.feature] is the value of the feature in the observation
+        if x[tree.feature] <= tree.threshold:
+            return self._predict(x, tree.data_left)
+        else:   # to right tree
+            return self._predict(x, tree.data_right)
         
-        # Go to the left
-        if feature_value <= tree.threshold:
-            return self._predict(x=x, tree=tree.data_left)
-        
-        # Go to the right
-        if feature_value > tree.threshold:
-            return self._predict(x=x, tree=tree.data_right)
-        
+    
     def predict(self, X):
-        '''
-        Function used to classify new instances.
-        
-        :param X: np.array, features
-        :return: np.array, predicted classes
-        '''
-        # Call the _predict() function for every observation
-        return [self._predict(x, self.root) for x in X]
+        """Used to predict the class of multiple observations.
+        Args:
+            X (np.array): features
+        Returns:
+            list: predicted classes
+        """
+        preds = []
+        for x in X:
+            # calls itself recursively
+            # TODO list comprehension
+            preds.append(self._predict(x, self.root))
+        return preds
 
 
 def main():
     iris = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris["target"], test_size=0.2, random_state=42)
+    # X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris["target"], test_size=0.2)
     model = DecisionTree()
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
