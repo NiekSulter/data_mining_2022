fb611f30806c152afde00ce2ccc5056364d0cb6f
Niek <54935153+NiekSulter@users.noreply.github.com>
2023-01-05 21:09:47 +0100

    chaos pt1: can calculate residuals and output values of the first tree using the base probability

--- GradientBoosting.py
+++ GradientBoosting.py
@@ -1,7 +1,8 @@
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, plot_tree
 from DecisionTreeRegression import DecisionTreeReg
 import numpy as np
-
+import time
+import matplotlib.pyplot as plt
 
 class GradientBoosting:
     def __init__(self, n_estimators, learning_rate, max_depth) -> None:
@@ -11,7 +12,7 @@ class GradientBoosting:
         
         self.trees = []
         for _ in range(self.n_estimators):
-            tree = DecisionTreeRegressor(max_depth=self.max_depth)
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, max_leaf_nodes=3)
             self.trees.append(tree)
             
         self.loss = CrossEntropy()
@@ -25,22 +26,62 @@ class GradientBoosting:
             X (_type_): _description_
             y (_type_): _description_
         """
-        class_0 = np.sum(y[:, 0], dtype='int')
-        class_1 = np.sum(y[:, 1], dtype='int')
+        class_0 = np.count_nonzero(y == 0)
+        class_1 = np.count_nonzero(y == 1)
         
         log_odds = np.log(class_1 / class_0)
         
-        probability = np.exp(log_odds) / (1 + np.exp(log_odds))
-        
-        pa = np.array([probability, 1 - probability])
+        initial_prediction = np.round(np.exp(log_odds) / (1 + np.exp(log_odds)), 1)
     
-        y_pred = np.full(np.shape(y), pa)
+        #calculated array with the residuals
+        residuals = np.full(np.shape(y), np.round(y - initial_prediction, 1))
+        
+        print(residuals)
+        print(initial_prediction)
         
         for i in range(self.n_estimators):
-            gradient = self.loss.gradient(y, y_pred)
-            self.trees[i].fit(X, gradient)
-            update = self.trees[i].predict(X)
-            y_pred -= np.multiply(self.learning_rate, update)
+            #train regression tree on residuals
+            self.trees[i].fit(X, residuals)
+            
+            plt.figure()
+            plot_tree(self.trees[i])
+            plt.savefig(f"tree_{i}.png")
+            
+            node_loc = self.trees[i].apply(X)
+            
+            leaves = dict((i, []) for i in np.unique(node_loc))
+            
+            for j in range(len(residuals)):
+                leaves[node_loc[j]].append(residuals[j])
+                
+            print(leaves)
+            
+            for key in leaves:
+                # output_value = (np.sum(leaves[key]) / np.sum([np.multiply(p, 1 - p) for p in leaves[key]]))
+                
+                output_value = (np.sum(leaves[key]) / np.sum([np.multiply(initial_prediction, 1 - initial_prediction) for p in leaves[key]]))
+                
+                
+                print(f"Leaf {key} has: {output_value}")
+            #update residuals with the predictions of the regression tree
+            # update = self.trees[i].predict(X)
+            
+            # # update_2 = np.array([(i / np.multiply(update[i], 1 - update[i])) for i in update])
+            
+            # update_2 = []
+            # for i in range(len(update)):
+            #     x = i / np.multiply(update[i], 1 - update[i])
+            #     update_2.append(x)
+            # update_2 = np.array(update_2)
+            
+            # #multiply the new residuals with the learning rate
+            # residuals += np.multiply(self.learning_rate, update_2)
+            
+            # new_probabilities = np.exp(residuals) / (1 + np.exp(residuals))
+            
+            # print(new_probabilities)
+            
+            
                 
     def predict(self, X):
         y_pred = np.array([])
@@ -49,7 +90,7 @@ class GradientBoosting:
             if not y_pred.any():
                 y_pred = -update
             else:
-                y_pred = y_pred - update
+                y_pred = np.subtract(y_pred, update)
             
         y_pred = np.exp(y_pred) / np.expand_dims(np.sum(np.exp(y_pred), axis=1), axis=1)
         
